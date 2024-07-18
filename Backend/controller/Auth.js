da0cf943dc142c1e3ee39fe7e849d9ec515dfad8
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-07-18 23:52:35 +0530

    succefully done the login controller with otp verifications

--- Backend/controller/Auth.js
+++ Backend/controller/Auth.js
@@ -1,7 +1,9 @@
 const Otp = require("../model/Otp");
 const User = require("../model/User");
 const otpGenerator = require("otp-generator");
-
+const bcrypt = require("bcrypt");
+const Profile = require("../model/Profile");
+const jwt = require("jsonwebtoken");
 // send otp code
 exports.sendOtp = async (req, res) => {
   try {
@@ -54,12 +56,150 @@ exports.sendOtp = async (req, res) => {
 
 // user register  controller code apply here
 exports.singup = async (req, res) => {
-  const { fistName, lastName, email, password, confirmPassword } = req.body;
-    
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      accountType,
+      contactNumber,
+      confirmPassword,
+      otp,
+    } = req.body;
 
+    // validattions, if anything is not be occured
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !otp
+    ) {
+      return res.status(404).json({
+        success: false,
+        message: "field is empty ! kindly fill it",
+      });
+    }
 
+    //   passwrod and confirm passwrod
+    if (password !== confirmPassword) {
+      return res.status(404).json({
+        success: false,
+        message: "password doesn't matched!",
+      });
+    }
 
+    //check if the users exist or not
+    const userExistOrNotCheckFromTheDb = User.findOne({ email: email });
+    if (userExistOrNotCheckFromTheDb) {
+      return res.status(404).json({
+        success: false,
+        message: "email already exist! kindly login it ",
+      });
+    }
+
+    // Find most recent otp using its time
+    const recentOtp = await Otp.find({ email: email })
+      .sort({ createdAt: -1 })
+      .limit(-1);
+    if (recentOtp.length == 0) {
+      return res.status(404).json({
+        success: false,
+        message: "otp is empty from server side",
+      });
+    } else if (otp !== recentOtp.otp) {
+      // check for invalid otp first
+      return res.status(404).json({
+        success: false,
+        message: "Invalid otp",
+      });
+    }
+    // start to hash the password
+    const hashPassword = await bcrypt.hash(password, 10);
+    const ProfileDetails = await Profile.create({
+      gender: null,
+      dateOFBirth: null,
+      about: null,
+      contactNumber: null,
+    });
+    // start to stroed the data into the db
+    const singupDataStoredToDb = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashPassword,
+      accountType,
+      contactNumber,
+      additionalDetails: ProfileDetails._id,
+      confirmPassword,
+      otp,
+      images: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName}${lastName}`,
+    });
+    return res.status(200).json({
+      success: true,
+      data: singupDataStoredToDb,
+      message: "Singup Succesfully done!",
+    });
+  } catch (er) {
+    return res.status(404).json({
+      success: false,
+      message: "error in SingUpcontroller !",
+      error: er.message,
+    });
+  }
 };
 
-//  singup
+// loginController
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(404).json({
+        success: false,
+        message: "field is empty !",
+        error: er.message,
+      });
+    }
+    // check the email
+    const checkEmailIfItsExistOrNot = await User.findOne({
+      email: email,
+    }).populate("additionalDetails");
+    if (checkEmailIfItsExistOrNot) {
+      return res.status(404).json({
+        success: false,
+        message: "email already exist here!",
+        error: er.message,
+      });
+    }
+    // password compared first
+
+    const passwordCompared = bcrypt.compare(
+      checkEmailIfItsExistOrNot.password,
+      password
+    );
+    if (!passwordCompared) {
+      return res.status(404).json({
+        success: false,
+        message: "Password didn't matched !",
+        error: er.message,
+      });
+    }
+
+    // now start to create the jwt token
+
+    // stored the data into the db
+    // const loginDataStoredToDb = await
+  } catch (er) {
+    return res.status(404).json({
+      success: false,
+      message: "Login failed in the controller !",
+      error: er.message,
+    });
+  }
+
+  // hash the passwod
+};
+
 // change password
