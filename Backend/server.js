7cc9178687a93a5f51670c5da94b062444bd7599
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-07-24 15:24:33 +0530

    connection done frontend +  backend

--- Backend/server.js
+++ Backend/server.js
@@ -25,6 +25,41 @@ app.use(express.json());
 // dbconnection
 dbConnectionFunctionCall();
 
+// cookie parser
+app.use(cookieParser());
+// call directly to the frontend
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+  })
+);
+
+// file upload middleware
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
+// connection to cloudinary
+MediaUploaderToCloud();
+
+// routes
+app.use("/api/v1/auth", UserRoutes);
+app.use("/api/v1/Courses", CourseRoutes);
+app.use("/api/v1/Profile", ProfileRoutes);
+app.use("/api/v1/Payment", PaymentRoutes);
+
+// defalut routes
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "server running well !",
+  });
+});
+
 // main listen port
 app.listen(PORT, () => {
   console.log(`server moving at this ${PORT}`);
